de75e1ed7aced248670810ef100c7320706df936
Melissa Patterson <melissa.patterson.va@gmail.com>
2015-01-09 11:07:30 -0600

    add list playlist function

--- music-snob.js
+++ music-snob.js
@@ -1,7 +1,35 @@
-
+Playlists = new Mongo.Collection('playlists')
 
 if (Meteor.isClient) {
+  Template.playlist.helpers({
+    playlists: function () {
+      return Playlists.find({});
+    }
+  });
+  
+/*
+  Template.body.events({
+  "submit .new-song": function (event) {
+    // This function is called when the new song form is submitted
 
+    var playlistName = event.target.playlistName.value;
+    var spotifyId =  
+    
+    Playlists.insert({
+      name: playlistName,
+      spotify_id: spotifyId,
+      songs: {}, 
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.playlistName.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});
+*/
 }
 
 
@@ -10,3 +38,4 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   
 }
+
