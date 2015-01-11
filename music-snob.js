8947ddf35ede247a6c1907c2745f896bad73f8a9
JMGoldsmith <spartanaudio@gmail.com>
2015-01-10 21:42:44 -0600

    some changes

--- music-snob.js
+++ music-snob.js
@@ -1,4 +1,5 @@
 Playlists = new Mongo.Collection('playlists');
+
 // client side logic
 if (Meteor.isClient) {
   Template.playlistSelect.helpers({
@@ -116,6 +117,20 @@ if (Meteor.isClient) {
 
 // server functions
 if (Meteor.isServer) {
+  Accounts.onCreateUser(function(options, user) {
+      // Here is the data this package gives you
+      var mks = user.services.makerpass;
+      // OPTIONAL: Restrict this app to "official" members of MakerSquare
+      if (mks.memberships.length === 0) {
+        throw new Meteor.Error(401, "Sorry, you are not a member of any MakerPass groups.");
+      }
+      // WARNING: user.profile is writable by user.
+      // Don't put something in user.profile if you don't want the user to change it.
+      user.profile = options.profile || {};
+      user.profile.name       = mks.name;
+      user.profile.avatarUrl  = mks.avatarUrl;
+      return user;
+  });
 	Meteor.methods({
 		searchTrack: function(track) {
 			var url = "https://api.spotify.com/v1/search?q=" + track + "&type=track&limit=10";
